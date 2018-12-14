e558b493d5a0ab4877a8de815ec780ebab29aee6
Connor Yates <cl.yates.42@gmail.com>
2018-12-14 10:42:57 -0800

    Initial pass at cleaning up and unifying the Gym Interface on the rover domain.
    Brings the interface more in line with the standard gym
    interface.
    
    Changes the gym_example and rover_domain_core_gym files to make the
    gym_example as simple as possible, which is in-line with the gym
    interface standard.

--- gym_example.py
+++ gym_example.py
@@ -1,88 +1,43 @@
-from rover_domain_core_gym import RoverDomainCoreGym
-from mods import *
-import datetime
-from code.world_setup import * # Rover Domain Construction 
-from code.agent_domain_2 import * # Rover Domain Dynamic  
-from code.trajectory_history import * # Agent Position Trajectory History 
-from code.reward_2 import * # Agent Reward 
-from code.reward_history import * # Performance Recording 
-from code.ccea_2 import * # CCEA 
-from code.save_to_pickle import * # Save data as pickle file
-import random
+"""
+An example using the rover domain gym-style interface and the standard, included CCEA learning algorithms.
+This is a minimal example, showing the minimal Gym interface.
+"""
+from rover_domain_core_gym import RoverDomainGym
+import code.ccea_2 as ccea
+#import code.reward_history as reward_hist
+import code.agent_domain_2 as domain
+import code.trajectory_history as traj_hist
 
+episodeCount = 1000  # Number of learning episodes
 
-stepCount = 5
-trainCountXEpisode = 3
-testCountXEpisode = 1
-episodeCount =  20
+sim = RoverDomainGym()
 
-# NOTE: Add the mod functions (variables) to run to modCol here:
-modCol = [
-    globalRewardMod,
-    differenceRewardMod,
-    dppRewardMod
-]
+#reward_hist.createRewardHistory(sim.data)
+ccea.initCcea(input_shape= 8, num_outputs=2, num_units = 32)(sim.data)
 
-i = 0
-while True:
-    print("Run %i"%(i))
-    random.shuffle(modCol)
-    for mod in modCol:
-        sim = RoverDomainCoreGym()
-        mod(sim)
-        
-        #Trial Begins
-        createRewardHistory(sim.data)
-        initCcea(input_shape= 8, num_outputs=2, num_units = 32)(sim.data)
-        sim.data["Steps"] = stepCount
-        
-        for episodeIndex in range(episodeCount):
-            sim.data["Episode Index"] = episodeIndex
-            
-            # Training Phase
-            
-            obs = sim.reset('Train', True)
-            
-            for worldIndex in range(trainCountXEpisode):
-                sim.data["World Index"] = worldIndex
-                obs = sim.reset('Train', False)
-                assignCceaPolicies(sim.data)
-                
-                done = False
-                stepCount = 0
-                while not done:
-                    doAgentProcess(sim.data)
-                    jointAction = sim.data["Agent Actions"]
-                    obs, reward, done, info = sim.step(jointAction)
-                    stepCount += 1
-                    
-                rewardCceaPolicies(sim.data)
-                    
-                    
-            # Testing Phase
-                    
-            obs = sim.reset('Test', True)
-            assignBestCceaPolicies(sim.data)        
-                    
-            for worldIndex in range(testCountXEpisode):
-                sim.data["World Index"] = worldIndex
-                obs = sim.reset('Test', False)
-                
-                done = False
-                stepCount = 0
-                while not done:
-                    doAgentProcess(sim.data)
-                    jointAction = sim.data["Agent Actions"]
-                    obs, reward, done, info = sim.step(jointAction)
-                    stepCount += 1
-                    
-            evolveCceaPolicies(sim.data)
-            updateRewardHistory(sim.data)
-            
-            # Trial End
-            saveRewardHistory(sim.data)
-            saveTrajectoryHistories(sim.data)
-        
-        
-        
-    i += 1
+for episodeIndex in range(episodeCount):
+    sim.data["Episode Index"] = episodeIndex
+    #sim.data["Performance Save File Name"] = "gym_example_performance"
+
+    obs = sim.reset()
+    ccea.assignCceaPolicies(sim.data)
+
+    done = False
+    stepCount = 0
+    while not done:
+        # Select actions and create the joint action from the simulation data
+        # Note that this specific function extracts "obs" from the data structure directly, which is why obs is not
+        # directly used in this example.
+        domain.doAgentProcess(sim.data)
+        jointAction = sim.data["Agent Actions"]
+        obs, reward, done, info = sim.step(jointAction)
+        stepCount += 1
+
+    ccea.rewardCceaPolicies(sim.data)
+    ccea.evolveCceaPolicies(sim.data)
+    #reward_hist.updateRewardHistory(sim.data)
+    print(sim.data["Global Reward"])
+
+    # Trial End
+    #reward_hist.saveRewardHistory(sim.data)
+    #traj_hist.saveTrajectoryHistories(sim.data)
