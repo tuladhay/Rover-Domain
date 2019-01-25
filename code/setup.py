bdd677b57c26be158a429417880f2a4a0d7fd414
Golden Rockefeller <rockefeg@oregonstate.edu>
2019-01-25 11:58:10 -0800

    Remove everything

--- code/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules = cythonize("agent_domain_2.pyx", annotate=True),
-    include_dirs=[numpy.get_include()]
-)
