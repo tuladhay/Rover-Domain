bae24a2317353805799f2535021229de26d76113
Golden Rockefeller <rockefeg@oregonstate.edu>
2019-01-29 11:11:28 -0800

    Record rover positions after each step call (Fixed)

--- rover_domain.pyx
+++ rover_domain.pyx
@@ -185,7 +185,8 @@ cdef class RoverDomain:
             # index because the initial position before any movement
             # is stored in rover_position_histories[0], so the first step
             # (step_id = 0) must be stored in rover_position_histories[1]
-            self.rover_position_histories[0,...] = self.init_rover_positions
+            self.rover_position_histories[self.step_id,...]\
+                = self.rover_positions
             
         self.done = self.step_id >= self.n_steps
         if evaluate:
