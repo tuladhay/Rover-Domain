95b9643fc200dedaf8a5c13ef259b2a15fe084c8
Connor Yates <yatesco@oregonstate.edu>
2019-02-01 15:09:42 -0800

    Update build process in setup
    Adds the right imports and the right GCC flags to the build file. Allows for a proper `python rover_domain_setup.py install` to build and install the package.

--- rover_domain_setup.py
+++ rover_domain_setup.py
@@ -1,34 +1,20 @@
+# Run setup
+from distutils.core import setup
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
-try:
-    # Run setup
-    from distutils.core import setup
-    from Cython.Build import cythonize
-    from Cython.Compiler import Options
+#Options.annotate = True
 
-    #Options.annotate = True
+setup(
+    name="rover_domain",
+    ext_modules=[
+        Extension('rover_domain',
+            sources=['rover_domain.pyx'],
+            extra_compile_args=['-std=c++11'],
+            language='c++')
+        ],
+    cmdclass = {'build_ext': build_ext}
+)
 
-    setup(
-        ext_modules=[
-            Extension('rover_domain',
-                sources=['rover_domain.pyx'],
-                extra_compile_args=['-std=c++11'],
-                language='c++')
-            ],
-        cmdclass = {'build_ext': build_ext}
-    )
-
-except:
-    from distutils.core import setup
-    from distutils.extension import Extension
-    from Cython.Distutils import build_ext
-
-    setup(
-        name = 'Test app',
-        ext_modules=[
-            Extension('rover_domain',
-                sources=['rover_domain.pyx'],
-                extra_compile_args=['-std=c++11'],
-                language='c++')
-            ],
-        cmdclass = {'build_ext': build_ext}
-    )
