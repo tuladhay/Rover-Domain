2958febb04f24965d28e567ee7ce3386656775b0
Connor Yates <yatesco@oregonstate.edu>
2019-02-03 19:58:17 -0800

    Add old CCEA/NN code, try to use for G/D baselines

--- baseline_tests.py
+++ baseline_tests.py
@@ -2,7 +2,8 @@ import numpy as np
 import random
 import pandas as pd
 import rover_domain
-
+import multi_poi_rover_domain
+import learning.ccea as ccea
 def test_G():
     """
     Creates a rover domain, initializes it, and uses CCEA to train a team using the global reward signal.
@@ -22,9 +23,18 @@ def test_G():
         print(np.array(obs), reward, done)
 
 
+def train_G():
+    """
+    Train on G
+    :return: None
+    """
+    srd = multi_poi_rover_domain.SequentialPOIRD()
+    process = ccea.Ccea(srd.n_rovers)
+
+
 
 if __name__ == "__main__":
     random.seed(0)
     np.random.seed(0)
 
-    test_G()
+
