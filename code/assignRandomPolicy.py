ef16e69f6934065acfb65382a57d64a4a5a8d8ed
ShawK91 <khadkas@oregonstate.edu>
2019-02-08 15:42:03 -0800

    MWC Python Rover-Domain
    TODOS
    1. compute_global_reward needs to be implemented
       - The func is already there and has access to rover_path, poi_pos and poi_value
    
    2. Orientation is not implemented

--- code/assignRandomPolicy.py
+++
@@ -1,9 +0,0 @@
-from policies.policy import RandomPolicy
-
-def assignRandomPolicy(data):
-    number_agents = data['Number of Agents']
-    policies = [None] * number_agents
-    
-    for agentIndex in range(data['Number of Agents']):
-        policies[agentIndex] = RandomPolicy(2)
-    worldData["Agent Policies"] = policies
